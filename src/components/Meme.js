61abfb1ae024142f2c68333ddf68071300ff9caa
Haemin Park <85291626+hampak@users.noreply.github.com>
2022-02-26 18:36:50 +0900

    Add files via upload

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,29 +4,69 @@ import memesData from "../memesData.js";
 
 export default function Meme() {
 	
-	const [memeImage, setMemeImage] = React.useState("")
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImg: "https://i.imgflip.com/1g8my4.jpg"
+	})
+	
+	const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+	
+	function handleChange(event) {
+		const {value, name} = event.target
+		setMeme(prevMeme => {
+			return {
+				...prevMeme,
+				[name]: value
+			}
+		})
+	}
+	
 	
 	function getMemeImage() {
-		const memesArray = memesData.data.memes
+		const memesArray = allMemeImages.data.memes
 		const randomNumber = Math.floor(Math.random() * memesArray.length)
-		setMemeImage(memesArray[randomNumber].url)
-
+		const url = memesArray[randomNumber].url
+		setMeme(prevMeme => ({
+			...prevMeme,
+			randomImg: url
+		}))
 	}
 	
 	return(
 		<div className="meme-section-container">
 			<div className="meme-sub-section">
-				<input className="top-text-input" type="text" placeholder="Top text"></input>
-				<input className="bottom-text-input" type="text" placeholder="Bottom text"></input>
+				<input 
+					className="top-text-input"
+					type="text" 
+					placeholder="Top text"
+					name="topText"
+					value={meme.topText}
+					onChange={handleChange}
+					></input>
+				<input 
+					className="bottom-text-input" 
+					type="text" 
+					placeholder="Bottom text"
+					name="bottomText"
+					value={meme.bottomText}
+					onChange={handleChange}
+					></input>
 			</div>
 			<button 
 				className="new-meme-btn"
 				onClick={getMemeImage}
 				
 				>Get a new meme image</button>
-			<img src={memeImage} className="meme-img"/>
+			<div className="meme-img-container">
+				<img src={meme.randomImg} className="meme-img"/>
+				<h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+			</div>
 		</div>
 	)
 }
 
-// https://youtu.be/bMknfKXIFA8?t=21947 까지 시청
+// https://youtu.be/bMknfKXIFA8?t=31865 까지 시청
+
+// https://scrimba.com/learn/learnreact/boxes-challenge-part-5-cobdb4d3e907fa304af4b9958
